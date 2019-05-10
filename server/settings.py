062a37f1f6d1b31689b6bc0acc4f2575f504872c
Damien <damien@dam.io>
2019-05-10 12:00:45 +0200

    remove ngrok hack added for demo

--- server/settings.py
+++ server/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("IODIDE_SERVER_DEBUG", default=False)
 
-SITE_URL = "http://b03edfea.ngrok.io" # env("SERVER_URI", default="http://localhost:8000/")
+SITE_URL = env("SERVER_URI", default="http://localhost:8000/")
 SITE_HOSTNAME = furl(SITE_URL).host
 ALLOWED_HOSTS = ['*']
 APP_VERSION_STRING = env.str("APP_VERSION_STRING", "dev")
